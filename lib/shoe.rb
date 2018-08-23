e782576b701be0998095e581784eeebd486ee7f9
Laura Darlak <laura@polyporedesign.com>
2018-08-23 15:11:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,9 @@ class Shoe
   def initialize(brand)
     @brand = brand
       # BRANDS << brand
+      # uniq! deletes all duplicates in an array
       # BRANDS.uniq!
+
       # alt
       BRANDS << brand unless BRANDS.include?(brand)
 
